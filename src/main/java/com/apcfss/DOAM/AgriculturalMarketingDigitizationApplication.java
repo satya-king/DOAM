3757cac19a09da443847afa6d8e19902af71299d
satya <satyanarayana.p@apcfss.in>
2025-09-01 15:07:12 +0530

    inital

---
+++ src/main/java/com/apcfss/DOAM/AgriculturalMarketingDigitizationApplication.java
@@ -0,0 +1,13 @@
+package com.apcfss.DOAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalMarketingDigitizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriculturalMarketingDigitizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apcfss/DOAM/AgriculturalMarketingDigitizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apcfss.DOAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalMarketingDigitizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
